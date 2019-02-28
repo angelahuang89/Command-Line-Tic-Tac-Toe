658534ddc33d81b15e1a5496310e401e2c2ddf54
Angela Huang <angelahuang89@gmail.com>
2019-02-27 23:04:41 -0800

    Update game logic

--- CLTicTacToe.js
+++ CLTicTacToe.js
@@ -19,6 +19,7 @@ class TicTacToe {
       8: 0,
       9: 0,
     };
+    this.totalMoves = 0;
     this.playerOne = {
       name: 'one',
       move: 'X'
@@ -59,26 +60,27 @@ class TicTacToe {
           if (this.board[i][j] === number) {
             this.board[i][j] = this.currentPlayer.move;
             this.hash[number] = this.currentPlayer.move;
+            this.totalMoves++;
+            if (this.checkGameOver()) return;
             this.switchPlayer();
             this.displayBoard();
             this.promptNextMove();
-            this.checkWin();
-            return;
           }
         }
       }
+    } else {
+      console.log('\nPlease enter a valid move.\n\n>>');
     }
-    console.log('\nPlease enter a valid move.\n\n>>');
   }
 
-  checkWin() {
+  checkGameOver() {
     const majorDiagonal = [];
     const minorDiagonal = [];
     for (let i = 0; i < this.board.length; i++) {
       const row = this.board[i];
       if (row.every(value => value === row[0])) {
-        this.displayWin(minorDiagonal[0]);
-        return;
+        this.displayWin(row[0]);
+        return true;
       }
 
       const column = [];
@@ -88,7 +90,7 @@ class TicTacToe {
 
       if (column.every(value => value === column[0])) {
         this.displayWin(column[0]);
-        return;
+        return true;
       }
 
       majorDiagonal.push(this.board[i][i]);
@@ -97,20 +99,29 @@ class TicTacToe {
 
     if (majorDiagonal.every(value => value === majorDiagonal[0])) {
       this.displayWin(majorDiagonal[0]);
-      return;
+      return true;
     }
 
     if (minorDiagonal.every(value => value === minorDiagonal[0])) {
       this.displayWin(minorDiagonal[0]);
-      return;
+      return true;
     }
+
+    if (this.totalMoves === 9) {
+      console.log('\nThe game ends in a stalemate.\n>>');
+      Game = null;
+      return true;
+    }
+
+    return false;
   }
 
   displayWin(winningMove) {
-    const winner = winningMove === 'X' ? 'one' : 'two';
-    console.log(`\nPlayer ${winner} wins!\n\n>>`);
+    const winner = winningMove === this.playerOne.move ? this.playerOne.name : this.playerTwo.name;
+    console.log(`\nPlayer ${winner} wins!`);
+    this.displayBoard();
+    console.log('\n>>');
     Game = null;
-    return;
   }
 };
 
@@ -123,20 +134,25 @@ process.stdin.on('data', text => {
 });
 
 const processUserInput = input => {
-  if (input === 'start\n') {
-    Game = new TicTacToe();
-    Game.displayBoard();
-    Game.promptNextMove();
-  } else if (input === 'options\n') {
-    console.log('\nCommands:\n\n\'start\' : start a new game\n\n\'quit\' : exit tic tac toe\n\n\'show board\' : show current state of the board\n\n\'current player\' : show current player\n\n>>');
-  } else if (input === 'current player\n') {
-    Game ? console.log(`\nThe current player is ${Game.currentPlayer.name}\n\n>>`) : console.log('\nThere is no game yet. Please type \'start\' to start one.\n\n>>');
-  } else if (input === 'show board\n') {
-    Game ? Game.displayBoard() + console.log('\n\n>>') : console.log('\nThere is no game yet. Please type \'start\' to start a new game.\n\n>>');
-  } else if (input === 'quit\n') {
-    exit();
-  } else {
-    Game ? Game.makeMove(input) : console.log('\nPlease enter a valid command.\n\nType \'options\' to see options.\n\n>>');
+  switch(input) {
+    case 'start\n':
+      Game = new TicTacToe();
+      Game.displayBoard();
+      Game.promptNextMove();
+      break;
+    case 'options\n':
+      console.log('\nCommands:\n\n\'start\' : start a new game\n\n\'quit\' : exit tic tac toe\n\n\'show board\' : show current state of the board\n\n\'current player\' : show current player\n\n>>');
+      break;
+    case 'current player\n':
+      Game ? console.log(`\nThe current player is ${Game.currentPlayer.name}.\n\n>>`) : console.log('\nThere is no game yet. Please type \'start\' to start one.\n\n>>');
+      break;
+    case 'show board\n':
+      Game ? Game.displayBoard() + console.log('\n\n>>') : console.log('\nThere is no game yet. Please type \'start\' to start a new game.\n\n>>');
+      break;
+    case 'quit\n':
+      exit();
+    default:
+      Game ? Game.makeMove(input) : console.log('\nPlease enter a valid command or type \'start\' to start a new game.\n\nType \'options\' to see options.\n\n>>');     
   }
 };
 
